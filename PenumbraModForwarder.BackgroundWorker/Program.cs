c6f296fbeca4cb59df1533d5e48346ab03c065d9
Dylan <41908416+ErrorDodo@users.noreply.github.com>
2024-11-28 21:21:48 +0800

    Get logging and DI working

--- PenumbraModForwarder.BackgroundWorker/Program.cs
+++
@@ -1,7 +0,0 @@
-using PenumbraModForwarder.BackgroundWorker;
-
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
-
-var host = builder.Build();
-host.Run();
