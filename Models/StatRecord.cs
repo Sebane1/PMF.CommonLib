afbc1ffced7d2b5c6d5d9005bbb1a6185892835e
Dylan <41908416+ErrorDodo@users.noreply.github.com>
2024-12-24 11:07:49 +0800

    Display unique installs

--- Models/StatRecord.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PenumbraModForwarder.Common.Models;
-
-public class StatRecord
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public int Count { get; set; }
-}
