ccb6bfe81572d35571064b9109fa082ebfba35f0
Dylan <41908416+ErrorDodo@users.noreply.github.com>
2025-01-31 09:11:18 +0800

    Move setting to correct model

--- CommonLib/Models/UIConfigurationModel.cs
+++ CommonLib/Models/UIConfigurationModel.cs
@@ -9,4 +9,6 @@ public class UIConfigurationModel
 
     [Display(Name = "Enable Notification Sound", GroupName = "Notification", Description = "Sound for Notifications")]
     public bool NotificationSoundEnabled { get; set; } 
+    [Display(Name = "Minimise To Tray", GroupName = "User Interface", Description = "Minimise the Main Window to tray")]
+    public bool MinimiseToTray { get; set; } = false;
 }
