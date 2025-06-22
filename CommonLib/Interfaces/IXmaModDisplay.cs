51768a0de68e44c26bd145d0b1cf761e7be227cb
Dylan <41908416+ErrorDodo@users.noreply.github.com>
2025-06-22 17:33:36 +0800

    XMA stuff has been moved to plugins

--- CommonLib/Interfaces/IXmaModDisplay.cs
+++
@@ -1,9 +0,0 @@
-﻿using CommonLib.Models;
-
-namespace CommonLib.Interfaces;
-
-public interface IXmaModDisplay
-{
-    Task<List<XmaMods>> GetRecentMods();
-    Task<string?> GetModDownloadLinkAsync(string modUrl);
-}
