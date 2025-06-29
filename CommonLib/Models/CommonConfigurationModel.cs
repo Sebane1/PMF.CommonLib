06626390ff536e17090bbcddf098d6697e54f1a6
Dylan <41908416+ErrorDodo@users.noreply.github.com>
2025-06-29 23:24:54 +0800

    Rework extraction to work based on file selection

--- CommonLib/Models/CommonConfigurationModel.cs
+++ CommonLib/Models/CommonConfigurationModel.cs
@@ -5,13 +5,13 @@ namespace CommonLib.Models;
 
 public class CommonConfigurationModel
 {
-    [Display(Name = "Enable Double Click File Support", GroupName = "General", Description = "Link double clicking files to running in PMF")]
+    [Display(Name = "Enable Double Click File Support", GroupName = "General", Description = "Link double clicking files to running in Atomos")]
     public bool FileLinkingEnabled { get; set; }
     [Display(Name = "Start on Boot", GroupName = "General", Description = "Start on Computer Boot")]
     public bool StartOnBoot { get; set; }
     [Display(Name = "Enable Beta Builds", GroupName = "Updates", Description = "Enable Beta Builds")]
     public bool IncludePrereleases { get; set; }
-    [Display(Name = "Start on FFXIV Boot", GroupName = "General", Description = "Put PMF into xivLauncher's config to make it run")]
+    [Display(Name = "Start on FFXIV Boot", GroupName = "General", Description = "Put Atomos into xivLauncher's config to make it run")]
     public bool StartOnFfxivBoot { get; set; }
     [Display(Name = "Enable Sentry", GroupName = "General", Description = "This will send crash logs to use so we can view them")]
     public bool EnableSentry { get; set; }
