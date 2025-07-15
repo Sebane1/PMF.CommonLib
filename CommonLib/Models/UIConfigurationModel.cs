144d0b6f9f644ba2d8c189a93f129a956ddef58e
Sebastina <sebane1@thavea.com>
2025-07-15 01:41:32 -0500

    Have options for minimizing and closing to tray.

--- CommonLib/Models/UIConfigurationModel.cs
+++ CommonLib/Models/UIConfigurationModel.cs
@@ -3,13 +3,14 @@ using CommonLib.Attributes;
 
 namespace CommonLib.Models;
 
-public class UIConfigurationModel
-{
+public class UIConfigurationModel {
     [Display(Name = "Enable Notifications", GroupName = "Notification", Description = "Display Notifications")]
     public bool NotificationEnabled { get; set; } = true;
 
     [Display(Name = "Enable Notification Sound", GroupName = "Notification", Description = "Sound for Notifications")]
     public bool NotificationSoundEnabled { get; set; }
+    [Display(Name = "Minimize To Tray", GroupName = "User Interface", Description = "Atomos will return to the tray when minimized.")]
+    public bool MinimizeToTray { get; set; } = false;
     [Display(Name = "Close To Tray", GroupName = "User Interface", Description = "Atomos will continue running after closing the window and be available in the tray")]
     public bool CloseToTray { get; set; } = false;
     [Display(Name = "Show Notification Button", GroupName = "User Interface", Description = "Show the Notification Button")]
